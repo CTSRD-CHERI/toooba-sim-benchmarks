ae4047cd4b6c9a68e0073d1ba31a32e40e8b7092
Jonathan Woodruff <jonwoodruff@gmail.com>
2025-07-03 12:15:48 +0100

    Correctly print column headings.

--- report_log.sh
+++ report_log.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Log, Cycles, Instructions
+echo "Log, Cycles, Instructions"
 for file in "$@"
 do
   echo "`basename ${file}`, `tac ${file} | grep -m 1 instret | sed 's/^instret:\([[:digit:]]*\).*   \([[:digit:]]*\)$/\2, \1/g'`"
