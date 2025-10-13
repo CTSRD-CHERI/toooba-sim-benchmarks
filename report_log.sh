ae14a12005c5e6b2f107cad7450344ee57ce4f9d
Daniel <danielgreenhome@gmail.com>
2025-10-13 15:05:23 +0100

    Pretty Printing of benchmark results

--- report_log.sh
+++ report_log.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-Log, Cycles, Instructions
-for file in "$@"
+csv_out=$1
+echo Log, Cycles, Instructions > $csv_out
+for file in "${@: 2}"
 do
-  echo "`basename ${file}`, `tac ${file} | grep -m 1 instret | sed 's/^instret:\([[:digit:]]*\).*   \([[:digit:]]*\)$/\2, \1/g'`"
+  echo "`basename ${file}`, `tac ${file} | grep -m 1 instret | sed 's/^instret:\([[:digit:]]*\).*   \([[:digit:]]*\)$/\2, \1/g'`" >> $csv_out
 done
+
+column -s, -t < $csv_out
